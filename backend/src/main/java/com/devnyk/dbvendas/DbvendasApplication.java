447f47add9f4da948bdde9aef8ef875184e90f06
nyksonalyson <nyksonalison@hotmail.com>
2021-05-11 20:14:33 -0300

    Project created

---
+++ backend/src/main/java/com/devnyk/dbvendas/DbvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devnyk.dbvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devnyk/dbvendas/DbvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devnyk.dbvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
